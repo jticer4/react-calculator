e5478cef944e9924a67d5774fd4d3fc8a1cf1714
jticer4 <jticer@cnm.edu>
2018-07-14 22:53:17 -0600

    added bootstrap and imported it index.js and App.js. I also added the btn class to all of my buttons to make sure it was working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './App.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 class Calculator extends React.Component {
   render() {
@@ -10,34 +11,34 @@ class Calculator extends React.Component {
          <div className="calculator-keypad">
 				<div className="input-keys">
 					<div className="function-keys">
-						<button className="calculator-key key-clear">AC</button>
-						<button className="calculator-key key-sign">+/-</button>
-						<button className="calculator-key key-percent">%</button>
-						<button className="calculator-key key-divide">/</button>
+						<button className="calculator-key key-clear btn">AC</button>
+						<button className="calculator-key key-sign btn">+/-</button>
+						<button className="calculator-key key-percent btn">%</button>
+						<button className="calculator-key key-divide btn">/</button>
 					</div>
 					<div className="digit-keys">
 						<div className="row-1">
-							<button className="calculator-key key-1">1</button>
-							<button className="calculator-key key-2">2</button>
-							<button className="calculator-key key-3">3</button>
-							<button className="calculator-key key-multiply">*</button>
+							<button className="calculator-key key-1 btn">1</button>
+							<button className="calculator-key key-2 btn">2</button>
+							<button className="calculator-key key-3 btn">3</button>
+							<button className="calculator-key key-multiply btn">*</button>
 						</div>
 						<div className="row-2">
-							<button className="calculator-key key-4">4</button>
-							<button className="calculator-key key-5">5</button>
-							<button className="calculator-key key-6">6</button>
-							<button className="calculator-key key-subtract">-</button>
+							<button className="calculator-key key-4 btn">4</button>
+							<button className="calculator-key key-5 btn">5</button>
+							<button className="calculator-key key-6 btn">6</button>
+							<button className="calculator-key key-subtract btn">-</button>
 						</div>
 						<div className="row-3">
-							<button className="calculator-key key-7">7</button>
-							<button className="calculator-key key-8">8</button>
-							<button className="calculator-key key-9">9</button>
-							<button className="calculator-key key-add">+</button>
+							<button className="calculator-key key-7 btn">7</button>
+							<button className="calculator-key key-8 btn">8</button>
+							<button className="calculator-key key-9 btn">9</button>
+							<button className="calculator-key key-add btn">+</button>
 						</div>
 						<div className="row-4">
-							<button className="calculator-key key-0">0</button>
-							<button className="calculator-key key-decimal">.</button>
-							<button className="calculator-key key-equals">=</button>
+							<button className="calculator-key key-0 btn">0</button>
+							<button className="calculator-key key-decimal btn">.</button>
+							<button className="calculator-key key-equals btn">=</button>
 						</div>
 					</div>
 				</div>
