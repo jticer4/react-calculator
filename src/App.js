2277d0988f790e9da1a1707ad0c60fbd80bcb3ec
jticer4 <jticer@cnm.edu>
2018-07-15 12:43:38 -0600

    added some bootstrap to make my buttons larger

--- src/App.js
+++ src/App.js
@@ -16,37 +16,37 @@ class Calculator extends React.Component {
 				<div className="input-keys">
 
 					<div className="function-keys row">
-						<button className="col-md-3 calculator-key key-clear btn btn-primary">AC</button>
-						<button className="col-md-3 calculator-key key-sign btn btn-info">+/-</button>
-						<button className="col-md-3 calculator-key key-percent btn btn-info">%</button>
-						<button className="col-md-3 calculator-key key-divide btn btn-warning">/</button>
+						<button className="col-md-3 calculator-key key-clear btn btn-primary btn-lg">AC</button>
+						<button className="col-md-3 calculator-key key-sign btn btn-info btn-lg">+/-</button>
+						<button className="col-md-3 calculator-key key-percent btn btn-info btn-lg">%</button>
+						<button className="col-md-3 calculator-key key-divide btn btn-warning btn-lg">/</button>
 					</div>
 
 						<div className="row-1 row">
-							<button className="col-md-3 calculator-key key-7 btn">7</button>
-							<button className="col-md-3 calculator-key key-8 btn">8</button>
-							<button className="col-md-3 calculator-key key-9 btn">9</button>
-							<button className="col-md-3 calculator-key key-multiply btn btn-warning">*</button>
+							<button className="col-md-3 calculator-key key-7 btn btn-lg">7</button>
+							<button className="col-md-3 calculator-key key-8 btn btn-lg">8</button>
+							<button className="col-md-3 calculator-key key-9 btn btn-lg">9</button>
+							<button className="col-md-3 calculator-key key-multiply btn btn-warning btn-lg">*</button>
 						</div>
 
 						<div className="row-2 row">
-							<button className="col-md-3 calculator-key key-4 btn">4</button>
-							<button className="col-md-3 calculator-key key-5 btn">5</button>
-							<button className="col-md-3 calculator-key key-6 btn">6</button>
-							<button className="col-md-3 calculator-key key-subtract btn btn-warning">-</button>
+							<button className="col-md-3 calculator-key key-4 btn btn-lg">4</button>
+							<button className="col-md-3 calculator-key key-5 btn btn-lg">5</button>
+							<button className="col-md-3 calculator-key key-6 btn btn-lg">6</button>
+							<button className="col-md-3 calculator-key key-subtract btn btn-warning btn-lg">-</button>
 						</div>
 
 						<div className="row-3 row">
-							<button className="col-md-3 calculator-key key-1 btn">1</button>
-							<button className="col-md-3 calculator-key key-2 btn">2</button>
-							<button className="col-md-3 calculator-key key-3 btn">3</button>
-							<button className="col-md-3 calculator-key key-add btn btn-warning">+</button>
+							<button className="col-md-3 calculator-key key-1 btn btn-lg">1</button>
+							<button className="col-md-3 calculator-key key-2 btn btn-lg">2</button>
+							<button className="col-md-3 calculator-key key-3 btn btn-lg">3</button>
+							<button className="col-md-3 calculator-key key-add btn btn-warning btn-lg">+</button>
 						</div>
 
 						<div className="row-4 row">
-							<button className="col-md-6 calculator-key key-0 btn">0</button>
-							<button className="col-md-3 calculator-key key-decimal btn">.</button>
-							<button className="col-md-3 calculator-key key-equals btn btn-warning">=</button>
+							<button className="col-md-6 calculator-key key-0 btn btn-lg">0</button>
+							<button className="col-md-3 calculator-key key-decimal btn btn-lg">.</button>
+							<button className="col-md-3 calculator-key key-equals btn btn-warning btn-lg">=</button>
 						</div>
 				</div>
 			</div>
