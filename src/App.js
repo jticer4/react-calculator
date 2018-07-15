b7374904d238845502700718ac295e4d7698c87a
jticer4 <jticer@cnm.edu>
2018-07-14 22:19:14 -0600

    refactored my html and added some padding to the calculator-display class

--- src/App.js
+++ src/App.js
@@ -13,35 +13,33 @@ class Calculator extends React.Component {
 						<button className="calculator-key key-clear">AC</button>
 						<button className="calculator-key key-sign">+/-</button>
 						<button className="calculator-key key-percent">%</button>
+						<button className="calculator-key key-divide">/</button>
 					</div>
 					<div className="digit-keys">
 						<div className="row-1">
 							<button className="calculator-key key-1">1</button>
 							<button className="calculator-key key-2">2</button>
 							<button className="calculator-key key-3">3</button>
+							<button className="calculator-key key-multiply">*</button>
 						</div>
 						<div className="row-2">
 							<button className="calculator-key key-4">4</button>
 							<button className="calculator-key key-5">5</button>
 							<button className="calculator-key key-6">6</button>
+							<button className="calculator-key key-subtract">-</button>
 						</div>
 						<div className="row-3">
 							<button className="calculator-key key-7">7</button>
 							<button className="calculator-key key-8">8</button>
 							<button className="calculator-key key-9">9</button>
+							<button className="calculator-key key-add">+</button>
 						</div>
 						<div className="row-4">
 							<button className="calculator-key key-0">0</button>
 							<button className="calculator-key key-decimal">.</button>
+							<button className="calculator-key key-equals">=</button>
 						</div>
 					</div>
-				</div>
-				<div className="operator-keys">
-					<button className="calculator-key key-divide">/</button>
-					<button className="calculator-key key-multiply">*</button>
-					<button className="calculator-key key-subtract">-</button>
-					<button className="calculator-key key-add">+</button>
-					<button className="calculator-key key-equals">=</button>
 				</div>
 			</div>
       </div>
